10976f8c512480a1c496cd2052b2644014bdffa9
matt <mdmccoy@gmail.com>
2018-01-22 12:41:22 -0500

    Refactor world.rb

--- voter_sim/spec/voter_spec.rb
+++
@@ -1,9 +0,0 @@
-require './voter'
-
-describe Voter do
-  context "All of the behaviors that a voter can do." do
-    it "When it is created it exits and has a political affiliation."
-
-    it "Has the correct political affiliation."
-  end
-end
