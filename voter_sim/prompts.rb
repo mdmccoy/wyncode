7ef3b6b4f841d33e36b8ccbf106e348dfc48ef45
mdmccoy <mdmccoy@gmail.com>
2018-01-22 21:30:40 -0500

    Refactor prompts.

--- voter_sim/prompts.rb
+++ voter_sim/prompts.rb
@@ -1,40 +1,21 @@
 module Prompts
   #puts prompt to screen, gets input and returns a single character
-  def main_menu_prompt
-    ask_downcase "\nWhat would you like to do?\n(C)reate, (L)ist, (U)pdate, (D)elete , or (Q)uit\n"
-  end
+  def main_menu_prompt() ask_downcase "\nWhat would you like to do?\n(C)reate, (L)ist, (U)pdate, (D)elete , or (Q)uit\n" end
 
-  def person_type
-    ask_downcase "\nWhat would you like to create?\n(P)olitician or (V)oter\n"
-  end
+  def person_type() ask_downcase "\nWhat would you like to create?\n(P)olitician or (V)oter\n" end
 
-  def get_name
-    ask "\nWhat is the name?\n"
-  end
+  def get_name() ask "\nWhat is the name?\n" end
 
-  def modify_person(modification)
-    ask "\nWho would you like to #{modification}?\n"
-  end
+  def modify_person(modification) ask "\nWho would you like to #{modification}?\n" end
 
-  def confirmation
-    ask_downcase "\nAre you sure? (Y/N)\n"
-  end
+  def confirmation() ask_downcase "\nAre you sure? (Y/N)\n" end
 
-  def politician_party
-    ask_downcase "\nWhat is the political party?\n(D)emocrat or (R)epublican?\n"
-  end
+  def politician_party() ask_downcase "\nWhat is the political party?\n(D)emocrat or (R)epublican?\n" end
 
-  def voter_party
-    ask_downcase "\nWhat is the political affiliation?\n(L)iberal, (C)onservative, (T)ea Party, (S)ocialist, (N)eutral?\n"
-  end
+  def voter_party() ask_downcase "\nWhat is the political affiliation?\n(L)iberal, (C)onservative, (T)ea Party, (S)ocialist, (N)eutral?\n" end
 
-  def ask_downcase(*question)
-    ask(*question).downcase
-  end
+  def ask_downcase(*question) ask(*question).downcase end
 
-  def ask(*question)
-    print(*question)
-    gets.chomp
-  end
+  def ask(*question) puts(*question); gets.chomp end
 
 end
