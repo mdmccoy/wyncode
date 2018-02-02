d02f5c742a3f174c2b162160641ea5a797045331
mdmccoy <mdmccoy@gmail.com>
2018-02-02 15:11:37 -0500

    Added loneliest chatroom hackathon.

--- web_advanced_and_js/js_attachment_checker/js_attachment_checker/server/public/script2.js
+++ web_advanced_and_js/js_attachment_checker/js_attachment_checker/server/public/script2.js
@@ -1,26 +1,12 @@
-// window.addEventListener('load', function() {
 $(function() {
-  // let buttons = document.getElementsByClassName("send");
   let $buttons = $('.send');
-  // for (let i = 0; i < buttons.length; i++) {
-  //   buttons[i].onclick = function() {
   $buttons.click(function() {
-    // let emailBody = document.getElementById('body').value.toLowerCase();
-    let emailBody = $('#body').val().toLowerCase();
-    // console.log(emailBody);
-    // let attachment = document.getElementsByName('attachment')[0].value.length;
-    let $attachment = $('#attachment').val().length;
-    // console.log($attachment);
-    // console.log($attachment);
-    // if ((emailBody.indexOf("attach") >= 0) && ($attachment === 0)) {
-    if (emailBody.includes("attach") && $attachment === 0) {
-      alert("You forgot your attachment!");
-      // let errorBox = document.getElementById("error");
-      let $errorBox = $('#error').css('display', 'block').text('You forgot your shit');
-      // $errorBox.css('display', 'block').text('You forgot your shit');
-      // $errorBox.text('You forgot your shit')
-      // $errorBox.style.display = 'block';
-      // $errorBox.innerText = "You forgot your attachment!";
+    let $emailBody = $('#body').val().toLowerCase(),
+      $attachment = $('#attachment').val().length,
+      msg = "You forgot you shit!";
+    if ($emailBody.includes("attach") && $attachment === 0) {
+      alert(msg);
+      let $errorBox = $('#error').fadeIn(2000).text(msg);
     }
   });
 
