f86641a17731d87c1f80034d2f7d6257c68e6095
mdmccoy <mdmccoy@gmail.com>
2018-02-03 13:30:48 -0500

    Refactor sendMessage to take an author. Add setAuthor.

--- web_advanced_and_js/lonely/loneliest-chatroom/script.js
+++ web_advanced_and_js/lonely/loneliest-chatroom/script.js
@@ -6,25 +6,9 @@ $(function() {
 
   let sendMessage = (author, message) => {
     let $convo = $('#conversation'),
-      timestamp = `${new Date().getHours()}:${new Date().getMinutes()}`,
-      greetings = ['Me', 'Myself', 'I'],
-      $author = $convo.find('.author').last().text();
-    switch ($author) {
-      case 'Me':
-        $author = 'Myself';
-        break;
-      case 'Myself':
-        $author = "I";
-        break;
-      case 'I':
-        $author = 'Me';
-        break;
-      default:
-        $author = 'Me'
-        break;
-    }
+      timestamp = `${new Date().getHours()}:${new Date().getMinutes()}`;
 
-    $convo.append(`<li class='message'><a class='delete' href='#'>Delete</a><h3 class="author">${$author}</h3><p class='message-body'>${message}</p><span class='timestamp'>${timestamp}</span></li>`)
+    $convo.append(`<li class='message'><a class='delete' href='#'>Delete</a><h3 class="author">${author}</h3><p class='message-body'>${message}</p><span class='timestamp'>${timestamp}</span></li>`)
     $messageBody.val("");
   };
 
@@ -33,25 +17,39 @@ $(function() {
   });
 
   $newMessageButton.click(function() {
-    sendMessage("test", $messageBody.val());
-
+    sendMessage(setAuthor(), $messageBody.val());
   });
 
   $messageBody.keydown(function(event) {
     if (event.keyCode === 13) {
-      sendMessage("test", $messageBody.val());
+      sendMessage(setAuthor(), $messageBody.val());
     }
   });
+
+  setAuthor = () => {
+    let $convo = $('#conversation');
+    author = $convo.find('.author').last().text();
+    switch (author) {
+      case 'Me':
+        author = 'Myself';
+        break;
+      case 'Myself':
+        author = "I";
+        break;
+      case 'I':
+        author = 'Me';
+        break;
+      default:
+        author = 'Me'
+        break;
+    }
+    return author;
+  };
 
   $lonely.click(function() {
     // JSON.parse($response.responseText).value.joke
     $.get('https://api.icndb.com/jokes/random', function(res, req) {
-      let $convo = $('#conversation'),
-        timestamp = `${new Date().getHours()}:${new Date().getMinutes()}`;
-      $convo.append(`<li class='message'><a class='delete' href='#'>Delete</a><h3 class="author">Internet</h3><p class='message-body'>${res.value.joke}</p><span class='timestamp'>${timestamp}</span></li>`);
-
+      sendMessage("Internet", res.value.joke);
     });
-
-
   });
 });
