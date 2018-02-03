2339eb8b12fdc0278d7f8a4e050dcdb40814da9f
mdmccoy <mdmccoy@gmail.com>
2018-02-03 09:28:22 -0500

    Working on delete button.

--- web_advanced_and_js/lonely/loneliest-chatroom/script.js
+++ web_advanced_and_js/lonely/loneliest-chatroom/script.js
@@ -1,11 +1,18 @@
 $(function() {
   let $newMessageButton = $('#new-message-button'),
-    $messageBody = $('#new-message-body')
-  sendMessage = () => {
-    let $convo = $('#conversation');
-    $convo.append(`<li class='message'><a class='delete' href='#'>Delete</a><h3 class="author">Me</h3><p class='message-body'>${$messageBody.val()}</p><span class='timestamp'>TIME</span></li>`)
-    $messageBody.val("");
-  };
+    $messageBody = $('#new-message-body'),
+    sendMessage = () => {
+      let $convo = $('#conversation');
+      $convo.append(`<li class='message'><a class='delete' href='#'>Delete</a><h3 class="author">Me</h3><p class='message-body'>${$messageBody.val()}</p><span class='timestamp'>TIME</span></li>`)
+      $messageBody.val("");
+      $deleteButton = $('a.delete');
+    },
+    $deleteButton = $('a.delete');
+  //this all the delete buttons
+
+  $deleteButton.click(function() {
+    console.log("I was clicked.");
+  });
 
   $newMessageButton.click(sendMessage);
 
