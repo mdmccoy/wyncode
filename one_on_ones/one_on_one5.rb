983318afa2a9aa9085b53a0cd03c0a137b4d55f1
mdmccoy <mdmccoy@gmail.com>
2018-02-03 19:13:49 -0500

    Add one on one

---
+++ one_on_ones/one_on_one5.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+
+directions = %w[north east south west]
+# p directions[3]
+
+compass = {
+  n: 'north',
+  e: 'east',
+  s: 'south',
+  w: 'west'
+}
+# p compass[:n]
+
+class Direction
+  attr_reader :abbreviation, :_direction_name
+
+  def initialize(abbreviation, _direction_name)
+    @abbreviation = abbreviation
+    @_direction_name = _direction_name
+  end
+end
+
+Direction.new('n', 'north')
+
+dir = Direction.new('n', 'North')
+# p dir, dir.abbreviation, dir._direction_name
+
+class Compass
+  attr_reader :directions
+
+  def initialize
+    @directions = [Direction.new('n', 'North'),
+                   Direction.new('e', 'East'),
+                   Direction.new('s', 'South'),
+                   Direction.new('w', 'West')]
+  end
+end
+
+my_compass = Compass.new
+p my_compass
+
+get '/' do
+  my_compass.to_s
+end
